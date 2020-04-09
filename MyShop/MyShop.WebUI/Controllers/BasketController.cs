9186ac023050bde426ea52b0cb223278ff9704c6
José Israel Flores Guzman <flores.guzman@gmail.com>
2020-04-08 22:46:23 -0500

    Created Basket UI

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketServices;
+
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketServices = BasketService;
+        }
+        
+        public ActionResult Index()
+        {
+            List<BasketItemViewModel> model = basketServices.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            basketServices.AddToBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            basketServices.RemoveFromBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketServices.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
